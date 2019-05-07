ce238fce1293ebf5832747be5a206d99a1ce25e1
Soraya Saenna <bubblebitoey@gmail.com>
2019-05-07 14:01:31 +0700

    avle to logout and separate fragment by buttomNav

--- app/src/main/java/recipes/soraya/com/SigninActivity.java
+++ app/src/main/java/recipes/soraya/com/SigninActivity.java
@@ -1,11 +1,11 @@
 package recipes.soraya.com;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.MenuItem;
 
 public class SigninActivity extends AppCompatActivity {
 	
@@ -13,16 +13,39 @@ public class SigninActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_signin);
-		Toolbar toolbar = findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
 		
-		FloatingActionButton fab = findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-			}
-		});
+		BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+		bottomNav.setOnNavigationItemSelectedListener(navListener);
+		
+		//I added this if statement to keep the selected fragment when rotating the device
+		        if (savedInstanceState == null) {
+			        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+		        }
 	}
+	
+	private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+ 
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_fav:
+                            selectedFragment = new FavFragment();
+                            break;
+                        case R.id.nav_setting:
+                            selectedFragment = new SettingFragment();
+                            break;
+                    }
+ 
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+ 
+                    return true;
+                }
+            };
 	
 }
