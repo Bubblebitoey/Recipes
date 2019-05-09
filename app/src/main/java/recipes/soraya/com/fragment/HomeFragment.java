a640d0381f354715fe441fc474acc53287d21bce
Piromsurang Rungserichai <piromsurang.r@ku.th>
2019-05-09 16:26:31 +0700

    able to read from json file

--- app/src/main/java/recipes/soraya/com/fragment/HomeFragment.java
+++ app/src/main/java/recipes/soraya/com/fragment/HomeFragment.java
@@ -2,16 +2,27 @@ package recipes.soraya.com.fragment;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import io.reactivex.Observable;
 import io.reactivex.subjects.Subject;
 import recipes.soraya.com.R;
+import recipes.soraya.com.models.Recipe;
 
 public class HomeFragment extends Fragment {
 
@@ -24,19 +35,36 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+
+        // load JSON recipes
+        new DownloadTasks().execute();
+
         return view;
     }
 
     private class DownloadTasks extends AsyncTask<Void, Void, Void> {
 
         private void readRecipesFromJSON() {
-            
+            String json = null;
+            try {
+                InputStream is = getActivity().getAssets().open("recipes.json");
+                int size = is.available();
+                byte[] buffer = new byte[size];
+                is.read(buffer);
+                is.close();
+                json = new String(buffer, "UTF-8");
+                HashMap<String,Object> result = new ObjectMapper().readValue(json, HashMap.class);
+                Log.e("JSON", result.size() + "");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
+
         @Override
         protected Void doInBackground(Void... voids) {
+            readRecipesFromJSON();
             return null;
         }
-    }
     }
 }
 
