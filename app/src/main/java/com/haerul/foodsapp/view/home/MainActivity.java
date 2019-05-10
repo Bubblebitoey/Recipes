0cbe4978393be7f1a5e55e67089e11f7e11b0f88
Soraya Saenna <bubblebitoey@gmail.com>
2019-05-10 18:16:05 +0700

    fix Sign Up problem

--- app/src/main/java/com/haerul/foodsapp/view/home/MainActivity.java
+++ app/src/main/java/com/haerul/foodsapp/view/home/MainActivity.java
@@ -3,12 +3,16 @@ package com.haerul.foodsapp.view.home;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import butterknife.ButterKnife;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
@@ -69,21 +73,21 @@ public class MainActivity extends AppCompatActivity implements MainView {
 	
 	@Override
 	public void callSignUp(String email, String password) {
-		mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
-			if (task.isSuccessful()) {
-				// Sign in success, update UI with the signed-in user's information
-				Toast.makeText(MainActivity.this, "SignUp Successful.", Toast.LENGTH_SHORT).show();
-				Log.d("d", "createUserWithEmail:success");
-				FirebaseUser user = mAuth.getCurrentUser();
-				userProfile(user);
-				// updateUI(user);
-				} else {
-				// If sign in fails, display a message to the user.
-				Toast.makeText(MainActivity.this, "SignUp Failed.", Toast.LENGTH_SHORT).show();
-				Log.w("d", "createUserWithEmail:failure", task.getException());
-			}
-			
-		});
+		mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+			@Override
+			public void onComplete(@NonNull Task<AuthResult> task) {
+				if (task.isSuccessful()) {// Sign in success, update UI with the signed-in user's information
+	
+					Toast.makeText(MainActivity.this, "SignUp Successful.", Toast.LENGTH_SHORT).show();
+					Log.d("d", "createUserWithEmail:success");
+					FirebaseUser user = mAuth.getCurrentUser();
+					userProfile(user); } else {
+					// If sign in fails, display a message to the user.
+					Toast.makeText(MainActivity.this, "SignUp Failed.", Toast.LENGTH_SHORT).show();
+					Log.w("d", "createUserWithEmail:failure", task.getException());
+	                        }
+	                    }
+	                });
 	}
 	
 	@Override
