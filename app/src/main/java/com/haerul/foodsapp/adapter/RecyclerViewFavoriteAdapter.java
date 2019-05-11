abc4e2fc87ae006122e61f9147acfa0b1deb77ce
Piromsurang Rungserichai <piromsurang.r@ku.th>
2019-05-11 10:35:06 +0700

    able to view favorite list and click to view details

--- app/src/main/java/com/haerul/foodsapp/adapter/RecyclerViewFavoriteAdapter.java
+++ app/src/main/java/com/haerul/foodsapp/adapter/RecyclerViewFavoriteAdapter.java
@@ -9,8 +9,10 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
 import com.haerul.foodsapp.R;
 import com.haerul.foodsapp.model.FavoriteRepository;
 import com.haerul.foodsapp.model.Meals;
@@ -19,82 +21,80 @@ import com.squareup.picasso.Picasso;
 import java.util.*;
 
 public class RecyclerViewFavoriteAdapter extends RecyclerView.Adapter<RecyclerViewFavoriteAdapter.RecyclerViewHolder> {
-	
-   
-	private static ClickListener clickListener;
-	private FavoriteRepository favoriteRepository;
-	private Context context;
-	private View view;
-	
-	public RecyclerViewFavoriteAdapter(Context context, ArrayList<Meals.Meal> favList) {
-	        this.context = context;
-	        this.favoriteRepository = FavoriteRepository.getInstance();
-	    }
-	
-	
-	@NonNull
-	@Override
-	public RecyclerViewFavoriteAdapter.RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-		view = LayoutInflater.from(context).inflate(R.layout.fragment_fav_recycler_meal, viewGroup, false);
-		return new RecyclerViewHolder(view);
-	}
-	
-	@Override
-	public void onBindViewHolder(@NonNull RecyclerViewFavoriteAdapter.RecyclerViewHolder recyclerViewHolder, int position) {
-		
-		
-		//TODO: bind favList into viewHolder
-		String strMealThumb = favoriteRepository.getFavoriteList().get(position).getStrMealThumb();
-		Picasso.get().load(strMealThumb).placeholder(R.drawable.shadow_bottom_to_top).into(recyclerViewHolder.mealThumb);
-		
-		String strMealName = favoriteRepository.getFavoriteList().get(position).getStrMeal();
-		recyclerViewHolder.mealName.setText(strMealName);
-	 
-		view.findViewById(R.id.delete).setOnClickListener(new View.OnClickListener() {
-	            @Override
-	            public void onClick(View view) {
-	                favoriteRepository.removeFromFavoriteList(favoriteRepository.getFavoriteList().get(position));
-	                showMessage("Sucessfully deleted from favorite.");
-	            }
-	        });
-	}
-	
-	@Override
-	public int getItemCount() {
-		return 0;
-	}
-	
-	static class RecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-		@BindView(R.id.mealThumb)
-		ImageView mealThumb;
-		@BindView(R.id.mealName)
-		TextView mealName;
-		
-		RecyclerViewHolder(@NonNull View itemView) {
-			super(itemView);
-			ButterKnife.bind(this, itemView);
-			itemView.setOnClickListener(this);
-		}
-		
-		@Override
-		public void onClick(View v) {
-			
-			clickListener.onClick(v, getAdapterPosition());
-		}
-		
-	}
-		
-		public void setOnItemListener(ClickListener clickListener) {
-			RecyclerViewFavoriteAdapter.clickListener = clickListener;
-		}
-		
-		public interface ClickListener {
-			void onClick(View v, int position);
-		}
-		
-		public void showMessage(String message) {
-			Toast.makeText(this.context.getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-		}
-		
-	
+
+
+    private static ClickListener clickListener;
+    private FavoriteRepository favoriteRepository;
+    private Context context;
+    private View view;
+
+    public RecyclerViewFavoriteAdapter(Context context) {
+        this.context = context;
+        this.favoriteRepository = FavoriteRepository.getInstance();
+    }
+
+
+    @NonNull
+    @Override
+    public RecyclerViewFavoriteAdapter.RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        view = LayoutInflater.from(context).inflate(R.layout.fragment_fav_recycler_meal, viewGroup, false);
+        return new RecyclerViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewFavoriteAdapter.RecyclerViewHolder recyclerViewHolder, int position) {
+
+
+        //TODO: bind favList into viewHolder
+        String strMealThumb = favoriteRepository.getFavoriteList().get(position).getStrMealThumb();
+        Picasso.get().load(strMealThumb).placeholder(R.drawable.shadow_bottom_to_top).into(recyclerViewHolder.mealThumb);
+
+        String strMealName = favoriteRepository.getFavoriteList().get(position).getStrMeal();
+        recyclerViewHolder.mealName.setText(strMealName);
+
+        view.findViewById(R.id.delete).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                favoriteRepository.removeFromFavoriteList(favoriteRepository.getFavoriteList().get(position));
+                showMessage("Sucessfully deleted from favorite.");
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return favoriteRepository.getFavoriteList().size();
+    }
+
+
+    public void setOnItemListener(ClickListener clickListener) {
+        RecyclerViewFavoriteAdapter.clickListener = clickListener;
+    }
+
+    public interface ClickListener {
+        void onClick(View v, int position);
+    }
+
+    public void showMessage(String message) {
+        Toast.makeText(this.context.getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    static class RecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        @BindView(R.id.mealThumb)
+        ImageView mealThumb;
+        @BindView(R.id.mealName)
+        TextView mealName;
+
+        RecyclerViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            clickListener.onClick(v, getAdapterPosition());
+        }
+
+    }
 }
--- app/src/main/java/com/haerul/foodsapp/view/home/fragment/FavoriteFragment.java
+++ app/src/main/java/com/haerul/foodsapp/view/home/fragment/FavoriteFragment.java
@@ -11,89 +11,119 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
 import butterknife.BindView;
+import butterknife.ButterKnife;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.haerul.foodsapp.R;
 import com.haerul.foodsapp.Utils;
 import com.haerul.foodsapp.adapter.RecyclerViewFavoriteAdapter;
+import com.haerul.foodsapp.adapter.RecyclerViewMealByCategory;
 import com.haerul.foodsapp.model.FavoriteRepository;
 import com.haerul.foodsapp.model.Meals;
+import com.haerul.foodsapp.view.home.category.CategoryPresenter;
 import com.haerul.foodsapp.view.home.detail.DetailActivity;
+import com.squareup.picasso.Picasso;
 
 import java.util.*;
 
 import static com.haerul.foodsapp.view.home.HomeActivity.EXTRA_DETAIL;
+import static com.haerul.foodsapp.view.home.HomeActivity.EXTRA_POSITION;
 
-public class FavoriteFragment extends Fragment implements FragmentView, Observer {
-	
-	
-	@BindView(R.id.favfromDB)
-	RecyclerView recyclerView;
-	@BindView(R.id.loadProgressBar)
-	ProgressBar progressBar;
-	
-	
-	
-	FavoriteRepository favoriteRepository = FavoriteRepository.getInstance();
-	RecyclerViewFavoriteAdapter adapter;
-	public static final String EXTRA_POSITION = "position";
-	private FirebaseAuth mAuth;
-	private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-	private DatabaseReference databaseReference;
-		
-		
-		@Nullable
-			@Override
-			public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-			View view = inflater.inflate(R.layout.fragment_fav_recycler_meal, container, false);
-			//ButterKnife.bind(this, view);
-			
-			favoriteRepository.addObserver(this);
-			
-			favoriteRepository.getFavoriteList();
-			
-			return view;
-		}
-	
-	@Override
-	public void update(Observable observable, Object o) {
-		//TODO: Don't know what to update
-		databaseReference = firebaseDatabase.getReference().child(mAuth.getCurrentUser().getUid());
-		
-	};
-	
-	//TODO: I try to setData from favoriteList
-	@Override
-	public void setMeals(ArrayList<Meals.Meal> favList) {
-			favoriteRepository = FavoriteRepository.getInstance();
-		RecyclerViewFavoriteAdapter adapter = new RecyclerViewFavoriteAdapter(getActivity(),favoriteRepository.getFavoriteList() );
-		recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
-		recyclerView.setClipToPadding(false);
-		recyclerView.setAdapter(adapter);
-		adapter.notifyDataSetChanged();
-		
-		adapter.setOnItemListener((view, position) -> {
-			
-			TextView mealName = view.findViewById(R.id.mealName);
-			Intent intent = new Intent(getActivity(), DetailActivity.class);
-			intent.putExtra(EXTRA_DETAIL, mealName.getText().toString());
-			intent.putExtra(EXTRA_POSITION, position);
-			
-			startActivity(intent);
-		});
-	}
-	
-	@Override
-	public void onErrorLoading(String message) {
-		Utils.showDialogMessage(getActivity(), "Error ", message);
-	}
+public class FavoriteFragment extends Fragment implements Observer {
+
+
+    @BindView(R.id.favfromDB)
+    RecyclerView recyclerView;
+    @BindView(R.id.textWelcomeUser)
+    TextView textWelcomeUser;
+
+
+    FavoriteRepository favoriteRepository = FavoriteRepository.getInstance();
+    RecyclerViewFavoriteAdapter adapter;
+    public static final String EXTRA_POSITION = "position";
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_fav_menucard, container, false);
+        ButterKnife.bind(this, view);
+
+        favoriteRepository.addObserver(this);
+
+        loadFavoriteList();
+
+        return view;
+    }
+
+    public void loadFavoriteList() {
+        adapter = new RecyclerViewFavoriteAdapter(getActivity());
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        recyclerView.setClipToPadding(false);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+
+        adapter.setOnItemListener(new RecyclerViewFavoriteAdapter.ClickListener() {
+            @Override
+            public void onClick(View v, int position) {
+                Intent intent = new Intent(getActivity(), DetailActivity.class);
+                intent.putExtra(EXTRA_DETAIL, favoriteRepository.getFavoriteList().get(position).getStrMeal());
+
+                intent.putExtra(EXTRA_POSITION, position);
+
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+    @Override
+    public void update(Observable observable, Object o) {
+        //TODO: Don't know what to update
+
+
+    }
+
+    ;
+
+    //TODO: I try to setData from favoriteList
+//	@Override
+//	public void setMeals(ArrayList<Meals.Meal> favList) {
+//			favoriteRepository = FavoriteRepository.getInstance();
+//		RecyclerViewFavoriteAdapter adapter = new RecyclerViewFavoriteAdapter(getActivity(),favoriteRepository.getFavoriteList() );
+//		recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+//		recyclerView.setClipToPadding(false);
+//		recyclerView.setAdapter(adapter);
+//		adapter.notifyDataSetChanged();
+//
+//		adapter.setOnItemListener((view, position) -> {
+//
+//			TextView mealName = view.findViewById(R.id.mealName);
+//			Intent intent = new Intent(getActivity(), DetailActivity.class);
+//			intent.putExtra(EXTRA_DETAIL, mealName.getText().toString());
+//			intent.putExtra(EXTRA_POSITION, position);
+//
+//			startActivity(intent);
+//		});
+//	}
+//
+//	@Override
+//	public void onErrorLoading(String message) {
+//		Utils.showDialogMessage(getActivity(), "Error ", message);
+//	}
 }
